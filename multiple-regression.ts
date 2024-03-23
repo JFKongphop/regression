826772ce434158f0045f84ff7b1feeccbdef1cc1
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-03-23 10:58:51 +0700

    create yhat

--- multiple-regression.ts
+++ multiple-regression.ts
@@ -13,17 +13,50 @@ class MultipleLinearRegression extends MatrixOperation {
     this.arraySize = y.length
   }
 
-
   getBeta(): number[] {
     const xTranspose = this.transpose(this.x);
-    const xMultiXtrans = this.multiplication(xTranspose, x);
+    const xMultiXtrans = this.multiplication(xTranspose, this.x);
     const xMultiXtransInv = this.inverse(xMultiXtrans);
-    const xTransMultiY = this.multiplication(xTranspose, y);
+    const xTransMultiY = this.multiplication(xTranspose, this.y);
     const betaMatrix = this.multiplication(xMultiXtransInv, xTransMultiY)
-    const beta = betaMatrix.reduce((acc, val) => acc.concat(val), []);
+    const beta = betaMatrix.reduce((row, num) => row.concat(num), []);
 
-    return beta
+    return beta;
   }
+
+  getYHat(x1: number, x2: number): number {
+    const beta = this.getBeta();
+    const yHat = beta[0] + (beta[1] * x1) + (beta[2] * x2);
+    
+    return yHat;
+  }
+
+//   ssr = 0
+// sse = 0
+// for i in range(5):
+//   y_hat = beta[0] + (beta[1] * x1[i]) + (beta[2] * x2[i])
+//   ssr += math.pow(y_hat - mean_y, 2)
+//   sse += math.pow(y[i] - y_hat, 2)
+  getSSE(): number {
+    let sse = 0;
+    for (let )
+    this.y.reduce((sum, num) => sum += (num[0]), 0)
+  }
+
+  // getSSR(): number {
+
+  // }
+
+  // sst = ssr + sse
+  // getSST(): number {
+  //   const sst = this.getSSR() + this.getSSE();
+  //   return sst;
+  // }
+
+  // getRSquare(): number {
+  //   const rSquare = this.getSSR() / this.getSST();
+  //   return rSquare;
+  // }
 }
 
 
@@ -43,6 +76,12 @@ const y: number[][] = [
 ]
 // const y: number[] = [-26.1, -31, -33.1, -25.8, -2.2]
 const multipleLinearRegression = new MultipleLinearRegression(x, y)
-console.log(multipleLinearRegression.getBeta(x, y))
+console.log(multipleLinearRegression.getBeta())
 
 
+
+const beta = x.reduce((row, num) => row.concat(num), []);
+console.log(beta)
+
+const t = x.map((row) => row.slice(1))
+console.log(t)
