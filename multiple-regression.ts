16027868a413008bd31617346ba5dbf31cee5813
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-03-23 11:53:11 +0700

    create ssr

--- multiple-regression.ts
+++ multiple-regression.ts
@@ -13,6 +13,17 @@ class MultipleLinearRegression extends MatrixOperation {
     this.arraySize = y.length
   }
 
+  private mean(x: number[]) {
+    const sum = x.reduce((sum, num) => sum += num, 0);
+    const mean = sum / x.length;
+
+    return mean;
+  }
+
+  private removeFirstColumn(x: number[][]) {
+    return x.map((row) => row.slice(1));
+  }
+
   getBeta(): number[] {
     const xTranspose = this.transpose(this.x);
     const xMultiXtrans = this.multiplication(xTranspose, this.x);
@@ -31,15 +42,9 @@ class MultipleLinearRegression extends MatrixOperation {
     return yHat;
   }
 
-//   ssr = 0
-// sse = 0
-// for i in range(5):
-//   y_hat = beta[0] + (beta[1] * x1[i]) + (beta[2] * x2[i])
-//   ssr += math.pow(y_hat - mean_y, 2)
-//   sse += math.pow(y[i] - y_hat, 2)
   getSSE(): number {
     let sse: number = 0;
-    const x1X2: number[][] = x.map((row) => row.slice(1));
+    const x1X2: number[][] = this.removeFirstColumn(this.x);
     
     for (let i = 0; i < this.arraySize; i++) {
       const yHat = this.getYHat(x1X2[i][0], x1X2[i][1]);
@@ -50,19 +55,28 @@ class MultipleLinearRegression extends MatrixOperation {
   }
 
   getSSR(): number {
+    let ssr: number = 0;
+    const y = this.y.reduce((row, num) => row.concat(num), []);
+    const yMean = this.mean(y);
+    const x1X2: number[][] = this.removeFirstColumn(this.x);
 
+    for (let i = 0; i < this.arraySize; i++ ) {
+      const yHat = this.getYHat(x1X2[i][0], x1X2[i][1]);
+      ssr += Math.pow(yHat - yMean, 2);
+    }
+
+    return ssr;
   }
 
-  // sst = ssr + sse
-  // getSST(): number {
-  //   const sst = this.getSSR() + this.getSSE();
-  //   return sst;
-  // }
+  getSST(): number {
+    const sst = this.getSSR() + this.getSSE();
+    return sst;
+  }
 
-  // getRSquare(): number {
-  //   const rSquare = this.getSSR() / this.getSST();
-  //   return rSquare;
-  // }
+  getRSquare(): number {
+    const rSquare = this.getSSR() / this.getSST();
+    return rSquare;
+  }
 }
 
 
@@ -83,6 +97,11 @@ const y: number[][] = [
 // const y: number[] = [-26.1, -31, -33.1, -25.8, -2.2]
 const multipleLinearRegression = new MultipleLinearRegression(x, y)
 console.log(multipleLinearRegression.getBeta())
+console.log(multipleLinearRegression.getSSR())
+console.log(multipleLinearRegression.getSSE())
+console.log(multipleLinearRegression.getSST())
+
+
 
 
 
@@ -92,4 +111,3 @@ console.log(beta)
 const t = x.map((row) => row.slice(1))
 console.log(t)
 
-this.y.reduce((sum, num) => sum += (num[0]), 0)
